c2c0d205fc36dfda423212cba9788c9ec0952c15
Ben Coepp <bencoepp@gmail.com>
2023-01-16 22:45:15 +0100

    removed some sources from the package

---
+++ src/main/java/de/bencoepp/entity/Test.java
@@ -0,0 +1,13 @@
+package de.bencoepp.entity;
+
+public class Test {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/de/bencoepp/utils/CommandHelper.java
@@ -0,0 +1,4 @@
+package de.bencoepp.utils;
+
+public class CommandHelper {
+}
